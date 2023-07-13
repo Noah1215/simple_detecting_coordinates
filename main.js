e178e6fe721b7837cbadf319fedfc54325fb412d
Chanwoong(Noah) Park <135056537+Noah1215@users.noreply.github.com>
2023-07-12 21:21:46 -0400

    upgrade-performance

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-const vertical = document.querySelector('.vertical');
-const horizontal = document.querySelector('.horizontal');
-const target = document.querySelector('.target');
-const tag = document.querySelector('.tag');
+const vertical = document.querySelector(".vertical");
+const horizontal = document.querySelector(".horizontal");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
 
-document.addEventListener('mousemove', (event)=> {
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const tartgetHalfWidth = targetRect.width / 2;
+  const tartgetHalfHeight = targetRect.height / 2;
+
+  document.addEventListener("mousemove", (event) => {
     const x = event.clientX;
     const y = event.clientY;
 
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
+    // vertical.style.left = `${x}px`;
+    // horizontal.style.top = `${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
 
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
+    // target.style.left = `${x}px`;
+    // target.style.top = `${y}px`;
+    const newX = x - tartgetHalfWidth;
+    const newY = y - tartgetHalfHeight;
+    target.style.transform = `translate(${newX}px, ${newY}px)`;
 
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
+    //   tag.style.left = `${x}px`;
+    //   tag.style.top = `${y}px`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
 
-    tag.innerHTML = `${x}px ,${y}px`
+    tag.innerHTML = `${x}px ,${y}px`;
+  });
 });
